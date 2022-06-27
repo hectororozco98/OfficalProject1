022ffbcf4453a44d1498249218e9c81010da3c78
Travis <travis795@revature.net>
2022-06-27 13:32:02 -0400

    Updating FrontController URI functionality and adding path for registration

--- src/main/java/com/revature/web/FrontController.java
+++ src/main/java/com/revature/web/FrontController.java
@@ -14,7 +14,8 @@ import javax.servlet.http.HttpServletResponse;
 public class FrontController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		final String URI = request.getRequestURI().replace("/official-project-one/", "");
+		//final String URI = request.getRequestURI().replace("/official-project-one/", "");
+		final String URI = request.getRequestURI().replace("/OfficialProject1/", "");
 		
 		switch(URI) {
 		case "login":
